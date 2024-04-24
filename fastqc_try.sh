f8287fbbba04c9b5649e7d4afb61ad82daec2004
Marie Hannaert <marie.hannaert@ilvo.vlaanderen.be>
2024-04-24 12:55:53 +0200

    Update fastqc_try.sh

--- fastqc_try.sh
+++ fastqc_try.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #loop over raw files
-
+mkdir output_fastqc
 for g in `ls *.fq.gz | awk 'BEGIN{FS=".fq.gz"}{print $1}'`
 do
     echo "quality control on $g with fastqc"
-    fastqc -o /home/mhannaert/01_fastqc --extract "$g".fq.gz #loop over read files
+    fastqc -o output_fastqc --extract "$g".fq.gz #loop over read files
 done
 echo
 echo "Finished fastqc"

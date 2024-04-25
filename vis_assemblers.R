3f5fa965db74c588f90bdade9fc06f3b28c30a9e
Marie Hannaert <marie.hannaert@ilvo.vlaanderen.be>
2024-04-25 16:38:53 +0200

    adding mini R visualizatie

---
+++ vis_assemblers.R
@@ -0,0 +1,15 @@
+#visualization of the comparison of three different assemblers in terms of the number of contigs and N50
+#install.packages("beeswarm")
+#making one figure with the two boxplots and two beeswarm plots
+par(mfrow = c(2,2))
+#the two boxplots
+boxplot(contigs ~ type_assemblers, data = quast_summary_table_complete, col= c("#3FA0FF", "#FFE099", "#F76D5E"))
+boxplot(N50 ~ type_assemblers, data = quast_summary_table_complete, col= c("#3FA0FF", "#FFE099", "#F76D5E"))
+
+library(beeswarm)
+beeswarm(contigs ~ type_assemblers, data = quast_summary_table_complete,
+         pch = 19, 
+         col = c("#3FA0FF", "#FFE099", "#F76D5E"))
+beeswarm(N50 ~ type_assemblers, data = quast_summary_table_complete,
+         pch = 19, 
+         col = c("#3FA0FF", "#FFE099", "#F76D5E"))

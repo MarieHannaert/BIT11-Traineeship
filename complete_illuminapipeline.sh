1833e95ea2825806965c78d5dbd77b7e29a6ad1f
Marie Hannaert <marie.hannaert@student.howest.be>
2024-05-07 17:23:21 +0200

    update with fastqc multiqc and error solving

--- complete_illuminapipeline.sh
+++ complete_illuminapipeline.sh
@@ -5,7 +5,7 @@
 
 ##checking for parameters
 function usage(){
-	errorString="Running this Kraken2 script requires 4 parameters:\n
+	errorString="Running this Illumina pipeline script requires 4 parameters:\n
     1. Path of the folder with fastq.gz files.\n
     2. Name of the output folder.\n
     3. Type of compression (gz or bz2)\n
@@ -44,29 +44,13 @@ done
 #going to the input directory
 cd $DIR
 
-#checking which file format
-#if bz then reformat to gz 
-#if gz then continue
-#if something else then exit, because needs to be gz of bz2
-if [[ $2 == "bz2" ]];
-    then
-        pbzip2 -d -p32 *fq.bz2 
-        pigz *.fq
-elif [[ $2 == "gz" ]];
-    then
-        continue
-else
-    echo "The compression type is not supported. Please use gz or bz2"
-    exit 1
-fi
-
 #making the output directory 
 mkdir -p $OUT
 
 #making a log file 
 touch "$OUT"/"$DATE_TIME"_Illuminapipeline.log
 #adding the user of the script that day to the log file 
-echo "The user of today is" $USER | tee -a "$OUT"/"$DATE_TIME"_Illuminapipeline.log
+echo "The user of" $DATE_TIME "is:" $USER | tee -a "$OUT"/"$DATE_TIME"_Illuminapipeline.log
 echo "====================================================================" | tee -a "$OUT"/"$DATE_TIME"_Illuminapipeline.log
 # adding the versions of the tools that are used 
 echo "the version that are used are:" | tee -a "$OUT"/"$DATE_TIME"_Illuminapipeline.log
@@ -77,6 +61,39 @@ echo "the command that was used is:"| tee -a "$OUT"/"$DATE_TIME"_Illuminapipelin
 echo "complete_illuminapipeline.sh" $1 $2 $3 $4 |tee -a "$OUT"/"$DATE_TIME"_Illuminapipeline.log
 echo "This was performed in the following directory: $START_DIR" |tee -a "$OUT"/"$DATE_TIME"_Illuminapipeline.log
 echo "====================================================================" | tee -a "$OUT"/"$DATE_TIME"_Illuminapipeline.log
+
+#checking which file format
+#if bz then reformat to gz 
+#if gz then continue
+#if something else then exit, because needs to be gz of bz2
+echo "checking fileformat and reformat if needed" | tee -a "$OUT"/"$DATE_TIME"_Illuminapipeline.log
+if [[ $3 == "bz2" ]]; then
+    #decompress bz2
+    echo "files are reformated to gz" | tee -a "$OUT"/"$DATE_TIME"_Illuminapipeline.log
+    pbzip2 -dk -p32 *fq.bz2 2>> "$OUT"/"$DATE_TIME"_Illuminapipeline.log
+    #compress to gz
+    pigz *.fq
+elif [[ $3 == "gz" ]]; then
+    echo "files are gz, so that's fine" | tee -a "$OUT"/"$DATE_TIME"_Illuminapipeline.log
+
+elif [[ $3 != "gz" ]] | [[ $3 != "bz2" ]]; then
+    echo "This is not a correct file format, it can only be gz or bz2" | tee -a "$OUT"/"$DATE_TIME"_Illuminapipeline.log
+    exit 1 2>> "$OUT"/"$DATE_TIME"_Illuminapipeline.log
+fi
+
+#performing fastqc on the gz samples 
+echo Performing fastqc | tee -a "$OUT"/"$DATE_TIME"_Illuminapipeline.log
+mkdir -p "$OUT"/fastqc
+fastqc -t 32 *.gz --extract -o "$OUT"/fastqc 
+#activating the mamba env 
+mamba init 2>> "$OUT"/"$DATE_TIME"_Illuminapipeline.log
+mamba activate multiqc 2>> "$OUT"/"$DATE_TIME"_Illuminapipeline.log
+#perfomring the multiqc on the fastqc samples
+echo performing multiqc | tee -a "$OUT"/"$DATE_TIME"_Illuminapipeline.log
+multiqc "$OUT"/fastqc 2>> "$OUT"/"$DATE_TIME"_Illuminapipeline.log
+#deactivating the mamba env
+mamba deactivate multiqc 2>> "$OUT"/"$DATE_TIME"_Illuminapipeline.log
+
 
 
 
